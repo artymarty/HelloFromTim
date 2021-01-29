105ad09c5bc0bf52bfc33520bb997f213668fb58
kylanrice <u870903242@byui.edu>
2021-01-29 15:10:38 -0700

    Added my name

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.print("Hello, my name is Timothy.");
+
+        System.out.println("Hello Timothy, my name is Kylan.");
     }
 }
